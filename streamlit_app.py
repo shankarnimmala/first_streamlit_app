3f2a0d5286d7588735affbad93c0b234cc5a599f
shankarnimmala <88205574+shankarnimmala@users.noreply.github.com>
2023-02-17 15:35:25 +0530

    custom order header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,8 @@ import streamlit
 streamlit.title('My Parents new Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('omlette')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
